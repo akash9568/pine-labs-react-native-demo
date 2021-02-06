defe6c08e76ef4b938b54bbb696bc11fd8fa71b4
pariwesh009 <ajay2322020@outlook.com>
2021-02-06 11:52:09 +0530

    created basic form

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { ScrollView, Switch, TextInput } from 'react-native-gesture-handler';
 
 export default function App() {
+  const [state, setState] = useState({ name: 'Pariwesh', age: '20', isEnabled: true });  //setState('Pariwesh')
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!233</Text>
-      <StatusBar style="auto" />
+      <ScrollView >
+        <Text>UserForm</Text>
+        <TextInput onChangeText={text => setState({ name: text })} style={{ borderColor: 'red', width: 300, borderWidth: 2 }} value={state.name}></TextInput>
+        <TextInput onChangeText={text => setState({ name: text })} style={{ borderColor: 'red', width: 300, borderWidth: 2 }} value={state.age}></TextInput>
+        <TextInput onChangeText={text => setState({ name: text })} style={{ borderColor: 'red', width: 300, borderWidth: 2 }} value={state.age}></TextInput>
+        <TextInput onChangeText={text => setState({ name: text })} style={{ borderColor: 'red', width: 300, borderWidth: 2 }} value={state.age}></TextInput>
+        <TextInput onChangeText={text => setState({ name: text })} style={{ borderColor: 'red', width: 300, borderWidth: 2 }} value={state.age}></TextInput>
+        <TextInput onChangeText={text => setState({ name: text })} style={{ borderColor: 'red', width: 300, borderWidth: 2 }} value={state.age}></TextInput>
+        <TextInput onChangeText={text => setState({ name: text })} style={{ borderColor: 'red', width: 300, borderWidth: 2 }} value={state.age}></TextInput>
+        <TextInput onChangeText={text => setState({ name: text })} style={{ borderColor: 'red', width: 300, borderWidth: 2 }} value={state.age}></TextInput>
+        <TextInput onChangeText={text => setState({ name: text })} style={{ borderColor: 'red', width: 300, borderWidth: 2 }} value={state.age}></TextInput>
+        <TextInput onChangeText={text => setState({ name: text })} style={{ borderColor: 'red', width: 300, borderWidth: 2 }} value={state.age}></TextInput>
+        <Switch trackColor={{ false: "#767577", true: "#81b0ff" }}
+          thumbColor={state.isEnabled ? "#f5dd4b" : "#f4f3f4"}
+          ios_backgroundColor="#3e3e3e" value={state.isEnabled}
+          onValueChange={(value) => setState({ isEnabled: value })}></Switch>
+        <Button title='Save' onPress={() => {
+          console.log('test');
+          alert(state);
+        }}></Button>
+        <StatusBar style="dark" />
+      </ScrollView>
     </View>
   );
 }
