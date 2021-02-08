2d38b09916bade4fbd2a6cfe08a298f217f68557
pariwesh009 <ajay2322020@outlook.com>
2021-02-08 10:27:19 +0530

    Added ListView Support

--- components/userform.js
+++ components/userform.js
@@ -4,8 +4,10 @@ import { Button, StyleSheet, Text, View } from 'react-native';
 import { ScrollView, Switch, TextInput } from 'react-native-gesture-handler';
 
 export default function Userform() {
-    console.log(useState({ name: 'Pariwesh', age: '20', isEnabled: true }))
-    const [state, setState] = useState({ name: 'Pariwesh', age: '20', isEnabled: true });  //setState('Pariwesh')
+    const [state, setState] = useState({
+        userform:{name: 'Pariwesh', age: '20', isEnabled: true},
+        users: [{ name: 'ajay' }]
+    });  //setState('Pariwesh')
     const URL = "https://6020a98046f1e400178034c6.mockapi.io/api/v1/users";
     function saveUser() {
         const promise = fetch(URL, {
@@ -13,28 +15,44 @@ export default function Userform() {
             headers: {
                 'content-type': 'application/json'
             },
-            body: JSON.stringify(state)
+            body: JSON.stringify(state.userform)
         });
-        promise.then(response => console.log(response))
+        promise.then(response => response.json()).then(user => {
+            console.log(user);
+            state.users.push(user);
+            setState({...state, users: state.users});
+        });
+        promise.catch(error => alert('Save operation failed'));
     }
     return (
         <View style={styles.container}>
             <ScrollView >
                 <Text>UserForm</Text>
-                <TextInput onChangeText={text => setState({ ...state, name: text })} style={{ borderColor: 'red', width: 300, borderWidth: 2 }} value={state.name}></TextInput>
-                <TextInput onChangeText={text => setState({ ...state, age: text })} style={{ borderColor: 'red', width: 300, borderWidth: 2 }} value={state.age}></TextInput>
-                <Switch trackColor={{ false: "#767577", true: "#81b0ff" }}
+                <TextInput onChangeText={text => setState({ ...state.userform, name: text })} style={{ borderColor: 'red', width: 300, borderWidth: 2 }} value={state.userform.name}></TextInput>
+                <TextInput onChangeText={text => setState({ ...state.userform, age: text })} style={{ borderColor: 'red', width: 300, borderWidth: 2 }} value={state.userform.age}></TextInput>
+                {/* <Switch trackColor={{ false: "#767577", true: "#81b0ff" }}
                     thumbColor={state.isEnabled ? "#f5dd4b" : "#f4f3f4"}
-                    ios_backgroundColor="#3e3e3e" value={state.isEnabled}
-                    onValueChange={(value) => setState({ isEnabled: value })}></Switch>
+                    ios_backgroundColor="#3e3e3e" value={state.userform.isEnabled}
+                    onValueChange={(value) => setState({ isEnabled: value })}></Switch> */}
                 <Button title='Save' onPress={saveUser}></Button>
                 <StatusBar style="dark" />
+                <ScrollView>{state.users.map((user, index) => {
+                    return <View style={styles.userRow}><Text>{user.name}</Text>
+                    </View>
+                })}</ScrollView>
             </ScrollView>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
+    userRow: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        borderColor: 'yellow',
+        borderWidth: 2,
+        backgroundColor: '#d2f455'
+    },
     container: {
         flex: 1,
         backgroundColor: '#fff',
