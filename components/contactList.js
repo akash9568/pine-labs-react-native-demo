6d46e6a15f8228463cf6cff8c32eb4b5cfa3586b
pariwesh009 <ajay2322020@outlook.com>
2021-02-15 10:52:54 +0530

    getting contact List on button click

--- components/contactList.js
+++ components/contactList.js
@@ -3,25 +3,36 @@ import { Button, SafeAreaView, Text, View } from "react-native";
 import * as Contacts from "expo-contacts";
 import { FlatList } from "react-native-gesture-handler";
 
-export default function ContactList(){
+export default function ContactList() {
     const [contacts, setContacts] = useState([]);
-    useEffect(()=> {
-        (async ()=> {
+    // useEffect(() => {
+    //     (async () => {
+    //         console.log('list is displayed');
+    //         const { status } = await Contacts.requestPermissionsAsync();
+    //         if (status === 'granted') {
+    //             const { data } = await Contacts.getContactsAsync();
+    //             if (data.length > 0) {
+    //                 // console.log(data);
+    //                 setContacts(data);
+    //             }
+    //         }
+    //     })();
+    // }, []);
+    function showList(){
+        const getContactsFunction = async () => {
             console.log('list is displayed');
-            const {status } = await Contacts.requestPermissionsAsync();
-            if(status === 'granted'){
-                const {data} = await Contacts.getContactsAsync();
-                if(data.length> 0 ){
-                    // console.log(data);
+            const { status } = await Contacts.requestPermissionsAsync();
+            if (status === 'granted') {
+                const { data } = await Contacts.getContactsAsync();
+                if (data.length > 0) {
                     setContacts(data);
                 }
             }
-        })();
+        }
+        getContactsFunction();
+    }
 
-    }, []);
-
-
-    function showContactDetails(item){
+    function showContactDetails(item) {
         alert(item.firstName)
     }
     return (
@@ -29,10 +40,13 @@ export default function ContactList(){
             <View>
                 <Text>this is a ContactList</Text>
             </View>
-            <FlatList data={contacts} renderItem={(item)=> <View><Text onPress={showContactDetails.bind(this, item.item)}>{item.item.firstName}</Text>
-            </View> }></FlatList>
+            <FlatList data={contacts} renderItem={(item) => {
+                console.log(item.item);
+                return <View><Text onPress={showContactDetails.bind(this, item.item)}>{item.item.firstName}</Text>
+                </View>
+            }}></FlatList>
+            <Button onPress={showList} title='showList'></Button>
 
-        
         </SafeAreaView>
     )
 }
